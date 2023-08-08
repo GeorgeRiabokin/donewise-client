300fc23687bc888d9f7339eeadd2901232de0177
George Riabokin <george.riabokin@gmail.com>
2023-08-08 10:55:10 +0300

    essential edits.

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,13 +1,2 @@
 import "./style.scss";
 import bg from "../../static/img/background-image.png"
-
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "Hello" + "webpack" + " ";
-
-  return element;
-}
-
-document.body.appendChild(component());
